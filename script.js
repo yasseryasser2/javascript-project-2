49cd0072f224a6d6385a2018c07a9250fa7c7199
Yasser Algburi <yasseralgburi@gmail.com>
2025-09-18 18:39:13 -0400

    loading bird title / random birds

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 //https://en.wikipedia.org/api/rest_v1/page/summary/
 
-//const birdName = document.getElementById("bird-name");
+const birdNameEl = document.getElementById("bird-name");
 const birdImg = document.getElementById("bird-img");
 
 const birdFact = document.getElementById("bird-fact");
-const randomButton = document.getElementById("random-btn");
+const randomButtonEl = document.getElementById("random-btn");
 
-const birds = ["Turkey vulture", "Barnacle goose", "Cape wagtail"];
+let birds = [];
 
-const fetchBirdName = async () => {
+async function loadBirdName() {
+  const res = await fetch("birds.txt");
+  const text = await res.text();
+  birds = text
+    .split("\n")
+    .map((line) => line.trim())
+    .filter(Boolean);
+}
+
+const loadBird = async () => {
   try {
-    const fetchBird = await fetch(
+    const randomElement = Math.floor(Math.random() * birds.length);
+    const randomBirdPicker = birds[randomElement];
+    const birdFetch = await fetch(
       `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(
-        birdName
+        randomBirdPicker
       )}`,
       {
         headers: {
-          accept: "application/json",
+          Accept: "application/json",
         },
       }
     );
-    const birdNameData = await fetchBird.json();
-    document.getElementById("bird-name").innerHTML = birdNameData.value;
-  } catch (error) {}
+
+    const birdData = await birdFetch.json();
+    birdNameEl.innerHTML = birdData.title;
+    console.log(birdData);
+  } catch (error) {
+    console.log(error);
+  }
 };
+await loadBirdName();
+loadBird();
+
+randomButtonEl.addEventListener("click", function () {
+  loadBird();
+});
